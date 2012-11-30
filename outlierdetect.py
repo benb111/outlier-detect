ee300acc9a7826a3f7bff2e058fe76a7cac34f04
Ben Birnbaum <benbirnbaum@Bens-MacBook-Pro-3.local>
2012-11-30 10:22:39 -0500

    Initial commit.

---
+++ outlierdetect.py
@@ -0,0 +1,225 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+outlierdetect.py
+
+Created by Ben Birnbaum on 2012-08-27.
+benjamin.birnbaum@gmail.com
+"""
+
+import collections
+import itertools
+import numpy as np
+import sys
+
+# Import optional dependencies
+_PANDAS_AVAILABLE = False
+try:
+    import pandas as pd
+    _PANDAS_AVAILABLE = True
+except ImportError:
+    pass
+_STATS_AVAILABLE = False
+try:
+    from scipy import stats
+    _STATS_AVAILABLE = True
+except ImportError:
+    sys.stderr.write('Cannot import scipy.  Some models may not be available.\n')
+    sys.stderr.flush()
+    pass
+
+
+############################################## Models ##############################################
+#
+# Each model must define a method with the signature compute_outlier_scores(frequencies).  The
+# parameter frequencies is a dictionary containing the distribution of response values for each
+# aggregation unit.  The method must return a dictionary giving the outlier score for each
+# aggregation unit.
+#
+# For example, the frequencies parameter might look like
+#
+# frequencies = {
+#     'interviewer1' : {
+#         'yes' : 23,
+#         'no'  : 12,
+#         'NA'  : 3
+#     },
+#     'interviewer2' : {
+#         'yes' : 8,
+#         'no'  : 25,
+#         'NA'  : 3
+#     }
+# }
+#
+# and the method might return something like
+# 
+# {
+#     'interviewer1' : 7.34,
+#     'interviewer2' : 2.30
+# }
+
+def _normalize_counts(counts, val=1):
+    """Helper function to normalize a dictionary of counts.
+
+    It normalizes the counts to add up to val.
+    counts should be a dictionary of the form {val1 : count1, val2 : count2, ...}.
+    Returns a dictionary of the same form.
+    """
+    n = sum([counts[k] for k in counts.keys()])
+    frequencies = {}
+    for r in counts.keys():
+        frequencies[r] = val * float(counts[r]) / float(n)
+    return frequencies
+
+
+if _STATS_AVAILABLE:
+    class MultinomialModel:
+        """Model to compute outlier scores based on the chi^2 test for multinomial data.
+    
+        See B. Birnbaum, B. DeRenzi, A. D. Flaxman, and N. Lesh. Automated quality control for mobile
+        data collection. In DEV ’12, pages 1:1–1:10, 2012.
+    
+        Requries scipy module.
+        """
+
+
+        def compute_outlier_scores(self, frequencies):
+            if len(frequencies.keys()) < 2:
+                raise Exception("There must be at least 2 aggregation units.")
+            rng = frequencies[frequencies.keys()[0]].keys()
+            outlier_scores = {}
+            for agg_unit in frequencies.keys():
+                expected_counts = _normalize_counts(
+                    self._sum_frequencies(agg_unit, frequencies),
+                    val=sum([frequencies[agg_unit][r] for r in rng]))
+                x2 = self._compute_x2_statistic(expected_counts, frequencies[agg_unit])
+                # logsf gives the log of the survival function (1 - cdf).
+                outlier_scores[agg_unit] = -stats.chi2.logsf(x2, len(rng) - 1)
+            return outlier_scores
+
+
+        def _compute_x2_statistic(self, expected, actual):
+            """Computes the X^2 statistic for observed frequencies.
+
+            Args:
+                expected: a dictionary giving the expected frequencies, e.g.,
+                    {'y' : 13.2, 'n' : 17.2}
+                actual: a dictionary in the same format as the expected dictionary
+                    (with the same range) giving the actual distribution.
+            """
+            rng = expected.keys()
+            if actual.keys() != rng:
+                raise Exception("Ranges of two frequencies are not equal.")
+            num_observations = sum([actual[r] for r in rng])
+            if abs(num_observations - sum([expected[r] for r in rng])) > 0.0001:
+                raise Exception("Frequencies must sum to the same value.")
+            return sum([(actual[r] - expected[r])**2 / max(float(expected[r]), 1.0)
+                for r in expected.keys()])
+
+
+        def _sum_frequencies(self, agg_unit, frequencies):
+            """Sums frequencies for each aggregation unit except the given one."""
+            # Get the range from the frequencies dictionary.  Assumes that the
+            # range is the same for each aggregation unit in this distribution.
+            rng = frequencies[agg_unit].keys()
+            all_frequencies = {}
+            for r in rng:
+                all_frequencies[r] = 0
+            for other_agg_unit in frequencies.keys():
+                if other_agg_unit == agg_unit:
+                    continue
+                for r in rng:
+                    all_frequencies[r] += frequencies[other_agg_unit][r]        
+            return all_frequencies
+
+
+class SValueModel:
+    """Computes s-value outlier scores.
+    
+    See B. Birnbaum, B. DeRenzi, A. D. Flaxman, and N. Lesh. Automated quality control for mobile
+    data collection. In DEV ’12, pages 1:1–1:10, 2012.
+    """
+
+
+    def compute_outlier_scores(self, frequencies):
+        if (len(frequencies.keys()) < 2):
+            raise Exception("There must be at least 2 aggregation units.")
+        rng = frequencies[frequencies.keys()[0]].keys()
+        normalized_frequencies = {}
+        for j in frequencies.keys():
+            normalized_frequencies[j] = _normalize_counts(frequencies[j])
+        medians = {}    
+        for r in rng:
+            medians[r] = np.median([normalized_frequencies[j][r]
+                for j in normalized_frequencies.keys()])
+        outlier_values = {}
+        for j in frequencies.keys():
+            outlier_values[j] = 0
+            for r in rng:
+                outlier_values[j] += abs(normalized_frequencies[j][r] - medians[r])
+        return self._normalize(outlier_values)
+    
+    
+    def _normalize(self, value_dict):
+        """Divides everything in value_dict by the median of values.
+
+        If the median is less than 1 / (# of aggregation units), it divides everything by
+        (# of aggregation units) instead.
+        """
+        median = np.median([value_dict[i] for i in value_dict.keys()])
+        n = len(value_dict.keys())
+        if median < 1.0 / float(n):
+            divisor = 1.0 / float(n)
+        else:
+            divisor = median
+        return_dict = {}
+        for i in value_dict.keys():
+            return_dict[i] = float(value_dict[i]) / float(divisor)
+        return return_dict
+
+
+########################################## Helper functions ########################################
+
+def _get_frequencies(data, col, col_vals, agg_col, agg_unit):
+    """TODO: comment."""
+    frequencies = {}
+    for col_val in col_vals:
+        frequencies[col_val] = 0
+        # (We can't just use collections.Counter() because frequencies.keys() is used to determine
+        # the range of possible values in other functions.)
+    if _PANDAS_AVAILABLE and isinstance(data, pd.DataFrame):
+        grouped = data[data[agg_col] == agg_unit].groupby(col)
+        for name, group in grouped:
+            frequencies[name] = len(group)
+    else:  # Assumes it is an np.ndarray
+        for row in itertools.ifilter(lambda row : row[agg_col] == agg_unit, data):
+            frequencies[row[col]] += 1
+    return frequencies
+
+
+def _run_alg(data, agg_col, cat_cols, model):
+    """TODO: comment."""
+    agg_units = sorted(np.unique(data[agg_col]))
+    outlier_scores = collections.defaultdict(dict)
+    for col in cat_cols:
+        col_vals = sorted(np.unique(data[col]))
+        frequencies = {}
+        for agg_unit in agg_units:
+            frequencies[agg_unit] = _get_frequencies(data, col, col_vals, agg_col, agg_unit)
+        outlier_scores_for_col = model.compute_outlier_scores(frequencies)
+        for agg_unit in agg_units:
+            outlier_scores[agg_unit][col] = outlier_scores_for_col[agg_unit]
+    return outlier_scores
+
+
+########################################## Public functions ########################################
+
+if _STATS_AVAILABLE:
+    def run_mma(data, aggregation_column, categorical_columns):
+        """TODO: comment."""
+        return _run_alg(data, aggregation_column, categorical_columns, MultinomialModel())
+
+
+def run_sva(data, aggregation_column, categorical_columns):
+    """TODO: comment."""
+    return _run_alg(data, aggregation_column, categorical_columns, SValueModel())
---
+++ test_outlierdetect.py
@@ -0,0 +1,188 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+test_outlierdetect.py
+
+Created by Ben Birnbaum on 2012-08-27.
+"""
+
+import unittest
+import numpy as np
+import pandas as pd
+
+from outlierdetect import *
+from outlierdetect import _get_frequencies
+from outlierdetect import _STATS_AVAILABLE
+
+
+def float_eq(a, b, eps=.001):
+    return abs(a - b) < eps
+
+
+if _STATS_AVAILABLE:
+    class TestMultinomialModel(unittest.TestCase):
+        def setUp(self):
+            self.model = MultinomialModel()
+
+        
+        def test_compute_outlier_scores(self):
+            frequencies = {
+                'a' : {'y' : 12, 'n' : 23, '-' : 11},
+                'b' : {'y' : 23, 'n' : 49, '-' : 39},
+                'c' : {'y' : 16, 'n' : 12, '-' : 14},
+            }
+            outlier_scores = self.model.compute_outlier_scores(frequencies)
+            self.assertEquals(sorted(outlier_scores.keys()), ['a', 'b', 'c'])
+            self.assertTrue(float_eq(outlier_scores['a'], 1.3593))
+            self.assertTrue(float_eq(outlier_scores['b'], 3.2995))
+            self.assertTrue(float_eq(outlier_scores['c'], 3.7355))
+    
+    
+        def test_numeric_response_values(self):
+            frequencies = {
+                'a' : {1 : 12, 2 : 23, 3 : 11},
+                'b' : {1 : 23, 2 : 49, 3 : 39},
+                'c' : {1 : 16, 2 : 12, 3 : 14},
+            }
+            outlier_scores = self.model.compute_outlier_scores(frequencies)
+            self.assertTrue(float_eq(outlier_scores['a'], 1.3593))
+            self.assertTrue(float_eq(outlier_scores['b'], 3.2995))
+            self.assertTrue(float_eq(outlier_scores['c'], 3.7355))
+
+
+class TestSValueModel(unittest.TestCase):
+    def setUp(self):
+        self.model = SValueModel()
+
+
+    def test_compute_outlier_scores(self):
+        frequencies = {
+            'a' : {'y' : 8,  'n' : 1,  '-' : 1},
+            'b' : {'y' : 14, 'n' : 4,  '-' : 2},
+            'c' : {'y' : 1,  'n' : 0,  '-' : 1},
+            'd' : {'y' : 9,  'n' : 1,  '-' : 0},
+            'e' : {'y' : 18, 'n' : 12, '-' : 0},
+        }
+        outlier_scores = self.model.compute_outlier_scores(frequencies)
+        self.assertEqual(sorted(outlier_scores.keys()), ['a', 'b', 'c', 'd', 'e'])
+        self.assertTrue(float_eq(outlier_scores['a'], .333333))
+        self.assertTrue(float_eq(outlier_scores['b'], .333333))
+        self.assertTrue(float_eq(outlier_scores['c'], 2.333333))
+        self.assertTrue(float_eq(outlier_scores['d'], 1))
+        self.assertTrue(float_eq(outlier_scores['e'], 1.6666667))
+    
+    
+    def test_numeric_response_values(self):
+        frequencies = {
+            'a' : {1 : 8,  2 : 1,  3 : 1},
+            'b' : {1 : 14, 2 : 4,  3 : 2},
+            'c' : {1 : 1,  2 : 0,  3 : 1},
+            'd' : {1 : 9,  2 : 1,  3 : 0},
+            'e' : {1 : 18, 2 : 12, 3 : 0},
+        }
+        outlier_scores = self.model.compute_outlier_scores(frequencies)
+        self.assertEqual(sorted(outlier_scores.keys()), ['a', 'b', 'c', 'd', 'e'])
+        self.assertTrue(float_eq(outlier_scores['a'], .333333))
+        self.assertTrue(float_eq(outlier_scores['b'], .333333))
+        self.assertTrue(float_eq(outlier_scores['c'], 2.333333))
+        self.assertTrue(float_eq(outlier_scores['d'], 1))
+        self.assertTrue(float_eq(outlier_scores['e'], 1.6666667))
+
+
+class TestGetFrequencies(unittest.TestCase):
+    def setUp(self):
+        self.data_rec_array = np.array([
+            ('a', 'yes'),
+            ('b', 'no'),
+            ('a', 'yes'),
+            ('a', 'yes'),
+            ('b', 'no' ),
+            ('a', 'no' ),
+        ], dtype=[('interviewer', 'a1'), ('question', 'a3')])
+        self.data_pandas = pd.DataFrame({
+            'interviewer' : ['a', 'b', 'a', 'a', 'b', 'a'],
+            'question'    : ['yes', 'no', 'yes', 'yes', 'no', 'no'],
+        })
+
+
+    def test_get_frequencies_rec_array(self):
+        self.assertEquals(
+            _get_frequencies(self.data_rec_array, 'question', ['yes', 'no'], 'interviewer', 'a'),
+            {'yes' : 3, 'no' : 1})
+        self.assertEquals(
+            _get_frequencies(self.data_rec_array, 'question', ['yes', 'no'], 'interviewer', 'b'),
+            {'yes' : 0, 'no' : 2})
+
+
+    def test_get_frequencies_pandas(self):
+        self.assertEquals(
+            _get_frequencies(self.data_pandas, 'question', ['yes', 'no'], 'interviewer', 'a'),
+            {'yes' : 3, 'no' : 1})
+        self.assertEquals(
+            _get_frequencies(self.data_pandas, 'question', ['yes', 'no'], 'interviewer', 'b'),
+            {'yes' : 0, 'no' : 2})
+
+
+class TestInterfaceFunctions(unittest.TestCase):
+    def setUp(self):
+        self.data_rec_array = np.array([
+            ('a', 'n', 'n'),
+            ('a', 'y', 'y'),
+            ('a', 'n', 'y'),
+            ('a', 'n', 'n'),
+            ('b', 'n', 'y'),
+            ('b', 'n', 'n'),
+            ('b', 'y', 'n'),
+            ('b', 'n', 'n'),
+            ('b', 'n', 'n'),
+            ('b', 'y', 'n'),
+            ('c', 'n', 'y'),
+            ('c', 'y', 'y'),
+            ('c', 'n', 'y'),
+            ('c', 'n', 'n'),
+            ('c', 'y', 'n'),
+            ('c', 'n', 'n'),
+        ], dtype=[('interviewer', 'a1'), ('q1', 'a1'), ('q2', 'a1')])
+        self.data_pandas = pd.DataFrame(self.data_rec_array)
+        self.q1_frequencies = {
+            'a' : {'y' : 1, 'n' : 3},
+            'b' : {'y' : 2, 'n' : 4},
+            'c' : {'y' : 2, 'n' : 4},
+        }
+        self.q2_frequencies = {
+            'a' : {'y' : 2, 'n' : 2},
+            'b' : {'y' : 1, 'n' : 5},
+            'c' : {'y' : 3, 'n' : 3},
+        }
+
+
+    def _test_function_using_model(self, f, model, data):
+        outlier_scores = f(data, 'interviewer', ['q1', 'q2'])
+        q1_scores = model.compute_outlier_scores(self.q1_frequencies)
+        q2_scores = model.compute_outlier_scores(self.q2_frequencies)
+        for interviewer in ['a', 'b', 'c']:
+            self.assertEquals(outlier_scores[interviewer]['q1'], q1_scores[interviewer])
+            self.assertEquals(outlier_scores[interviewer]['q2'], q2_scores[interviewer])
+
+
+    if _STATS_AVAILABLE:
+        def test_run_mma(self):
+            self._test_function_using_model(run_mma, MultinomialModel(), self.data_rec_array)
+            self._test_function_using_model(run_mma, MultinomialModel(), self.data_pandas)
+
+
+    def test_run_sva(self):
+        self._test_function_using_model(run_sva, SValueModel(), self.data_rec_array)
+        self._test_function_using_model(run_sva, SValueModel(), self.data_pandas)
+
+
+    def test_works_when_some_interviewers_are_missing_values(self):    
+        run_sva(self.data_rec_array[2:], 'interviewer', ['q1'])
+        run_sva(self.data_pandas[2:], 'interviewer', ['q1'])
+        if _STATS_AVAILABLE:
+            run_mma(self.data_rec_array[2:], 'interviewer', ['q1'])
+            run_mma(self.data_pandas[2:], 'interviewer', ['q1'])
+
+
+if __name__ == '__main__':
+    unittest.main()
