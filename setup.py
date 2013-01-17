71539b322358109fee914d097babeba8cdbb119b
A. Flaxman <abie@alum.mit.edu>
2013-01-17 11:55:51 -0800

    ENH: make package installable with distutils

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='outlierdetect',
+      version='0.1',
+      description='Outlier detection algorithms',
+      author='Ben Birnbaum',
+      author_email='benjamin.birnbaum@gmail.com',
+      url='http://github.com/benb111/outlier-detect',
+      packages=['outlierdetect'],
+      )
